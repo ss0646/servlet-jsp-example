cb763e12634ef45da3967f7872ce66a69d1cbe60
ss0646 <>
2023-01-06 19:45:26 +0900

    Filter動作確認

--- src/main/java/servlet/FormSampleServlet.java
+++ src/main/java/servlet/FormSampleServlet.java
@@ -21,7 +21,7 @@ public class FormSampleServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
+//		request.setCharacterEncoding("UTF-8");
 		String name = request.getParameter("name");
 		String gender = request.getParameter("gender");
 
