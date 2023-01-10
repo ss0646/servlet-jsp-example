41576bf598e56c19ac2418c12428e0b94c2583b9
ss0646 <>
2023-01-10 18:00:27 +0900

    JDBCプログラムを隠蔽

--- src/main/java/SelectEmployeeSample.java
+++ src/main/java/SelectEmployeeSample.java
@@ -1,28 +1,18 @@
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.List;
+
+import dao.EmployeeDAO;
+import model.Employee;
 
 public class SelectEmployeeSample {
 
 	public static void main(String[] args) {
-		try (Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/example", "sa", "")) {
-			String sql = "SELECT ID, NAME, AGE FROM EMPLOYEE";
-			PreparedStatement pSmnt = conn.prepareStatement(sql);
-			ResultSet rs = pSmnt.executeQuery();
-			while (rs.next()) {
-				String id = rs.getString("ID");
-				String name = rs.getString("NAME");
-				int age = rs.getInt("AGE");
-
-				System.out.println("ID:" + id);
-				System.out.println("NAME:" + name);
-				System.out.println("AGE:" + age);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
+		EmployeeDAO empDAO = new EmployeeDAO();
+		List<Employee> empList = empDAO.findAll();
+		for (Employee emp : empList) {
+			System.out.println("ID:" + emp.getId());
+			System.out.println("NAME:" + emp.getName());
+			System.out.println("AGE:" + emp.getAge());
 		}
 	}
 
